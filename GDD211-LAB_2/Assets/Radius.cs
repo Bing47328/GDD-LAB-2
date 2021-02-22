ebc654a29d73582e7644db86484f350bdd189c47
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-02-22 12:34:21 -0500

    Unity

--- GDD211-LAB_2/Assets/Radius.cs
+++ GDD211-LAB_2/Assets/Radius.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Radius : MonoBehaviour
 {
     public GameObject input;
+    public GameObject playerText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             input.SetActive(true);
+            playerText.SetActive(true);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         input.SetActive(false);
+        playerText.SetActive(false);
     }
 }
