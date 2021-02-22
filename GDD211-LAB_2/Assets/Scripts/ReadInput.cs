8d650dd96171976b3379c27450d6cfa1f73e8e55
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-02-22 01:25:04 -0500

    Unity

--- GDD211-LAB_2/Assets/Scripts/ReadInput.cs
+++ GDD211-LAB_2/Assets/Scripts/ReadInput.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadInput : MonoBehaviour
 {
     private string input;
+    public GameObject panel;
+    public Text user;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +17,16 @@ public class ReadInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        user.text = input;
     }
 
     public void ReadStringInput(string x)
     {
         input = x;
-
+        //if (Input.GetKey(KeyCode.Return))
+        //{
+        //    panel.SetActive(false);
+        //}
+        Debug.Log(input);
     }
 }
