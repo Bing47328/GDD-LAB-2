3c559803c75da5d6e9501b3a28a0dfec3b0ce61c
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-02-22 12:27:47 -0500

    Update Response.cs

--- GDD211-LAB_2/Assets/Response.cs
+++ GDD211-LAB_2/Assets/Response.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Response : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string response;
+    public GameObject inputField;
+    public GameObject textDisplay;
 }
